2904b9fe283991b41d856b0f8a21632ac32e527d
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-08-08 18:59:46 -0500

    clase 1

---
+++ clase_1/index.js
@@ -0,0 +1,18 @@
+var name = "hola";
+console.log(typeof name);
+
+// var el scope es global
+// let es solo para el bloque
+// const no se puede sobreescribir (depende del scope)
+
+function suma() {
+  const edad = 20;
+    
+  if (true) {
+    const edad = 30;
+    console.log(edad);
+  }
+  console.log(edad);
+}
+
+console.log(suma());
